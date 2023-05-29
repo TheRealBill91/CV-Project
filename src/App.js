1b07e36a6baebbcf287d50671e6a45b671fe9732
Billy <bc35786@gmail.com>
2023-05-29 18:16:01 -0500

    Refactor & finish most of General Info section

--- src/App.js
+++ src/App.js
@@ -6,76 +6,12 @@ import uniqid from "uniqid";
 export default class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      generalData: {
-        nameInput: "",
-        emailInput: "",
-        phoneInput: "",
-        id: uniqid(),
-      },
-      generalDataArr: [],
-    };
-
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePhoneNumChange = this.handlePhoneNumChange.bind(this);
-    this.submitGeneralInfo = this.submitGeneralInfo.bind(this);
-  }
-
-  handleNameChange(e) {
-    this.setState({
-      generalData: {
-        nameInput: e.target.value,
-      },
-    });
-  }
-
-  handleEmailChange(e) {
-    this.setState({
-      generalData: {
-        emailInput: e.target.value,
-      },
-    });
-  }
-
-  handlePhoneNumChange(e) {
-    this.setState({
-      generalData: {
-        phoneInput: e.target.value,
-      },
-    });
-  }
-
-  submitGeneralInfo(e) {
-    e.preventDefault();
-    this.setState({
-      generalDataArr: this.state.generalDataArr.concat({
-        name: this.state.nameInput,
-        email: this.state.emailInput,
-        phoneNumber: this.state.phoneInput,
-      }),
-      generalData: {
-        nameInput: "",
-        emailInput: "",
-        phoneInput: "",
-        id: uniqid(),
-      },
-    });
   }
 
   render() {
-    const { generalData, generalDataArr } = this.state;
     return (
       <>
-        <FormOverview
-          generalData={generalDataArr}
-          generalInput={generalData}
-          handleNameChange={this.handleNameChange}
-          handleEmailChange={this.handleEmailChange}
-          handlePhoneNumChange={this.handlePhoneNumChange}
-          submitGeneralInfo={this.submitGeneralInfo}
-        ></FormOverview>
+        <FormOverview />
       </>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { GeneralInfoSection } from "./GeneralInfo";
 import "../styles/FormSection.css";
 
 // Parent component for form
@@ -8,68 +9,12 @@ export class FormOverview extends Component {
     }
 
     render() {
-        const {
-            generalData,
-            generalDataArr,
-            handleNameChange,
-            handleEmailChange,
-            handlePhoneNumChange,
-            submitGeneralInfo,
-        } = this.props;
         return (
             <>
-                <h3>General Information</h3>
-                <GeneralInfoSection
-                    generalData={generalDataArr}
-                    generalInput={generalData}
-                    handleNameChange={handleNameChange}
-                    handleEmailChange={handleEmailChange}
-                    handlePhoneNumChange={handlePhoneNumChange}
-                    submitGeneralInfo={submitGeneralInfo}
-                ></GeneralInfoSection>
+                <GeneralInfoSection></GeneralInfoSection>
             </>
         );
     }
 }
 
-// Component for rendering each of the three form sections
-// general info, education info, and practical info (past experience)
 
-class GeneralInfoSection extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const {
-            generalData,
-            generalInput,
-            handleNameChange,
-            handleEmailChange,
-            handlePhoneNumChange,
-            submitGeneralInfo,
-        } = this.props;
-        return (
-            <>
-                <div className="#GeneralInfo">
-                    <form>
-                        <label>Enter full name:</label>
-                        <input
-                            placeholder="John Appleseed"
-                            type="text"
-                            value={generalData}
-                            onChange={handleNameChange}
-                        ></input>
-                        <label>Enter your email address:</label>
-                        <input
-                            placeholder="JohnAppleseed@aol.com"
-                            type="email"
-                            value={generalData}
-                            onChange={handleEmailChange}
-                        ></input>
-                    </form>
-                </div>
-            </>
-        );
-    }
-}
